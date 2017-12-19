142f51bfea7c13f1e7e2d2f9d18337d81ac56419
Aryel Mota Góis <aryel.gois@gmail.com>
2017-12-19 13:04:20 -0300

    Add test files

---
+++ tests/yasql.php
@@ -0,0 +1,10 @@
+#!/usr/bin/env php
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$database = file_get_contents(__DIR__ . '/database.yml');
+
+$yasql = new aryelgois\YaSql\YaSql($database);
+
+var_dump($yasql);
