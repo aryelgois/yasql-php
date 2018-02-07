36560d155df9b668a2cc005f96ad17487e5b23ae
Aryel Mota Góis <aryel.gois@gmail.com>
2018-02-07 18:11:44 -0300

    Remove Populator->root

--- src/Populator.php
+++ src/Populator.php
@@ -50,30 +50,13 @@ abstract class Populator
     protected $filename;
 
     /**
-     * Root directory to load files
-     *
-     * @var string
-     */
-    protected $root;
-
-    /**
-     * Creates a new Populator object
-     *
-     * @param string $root Root directory to load files
-     */
-    public function __construct(string $root)
-    {
-        $this->root = $root;
-    }
-
-    /**
      * Loads a YAML file to be processed
      *
      * @param string $file Path to YAML source file
      */
     public function load(string $file)
     {
-        $this->data = Yaml::parse(file_get_contents($this->root . '/' . $file));
+        $this->data = Yaml::parse(file_get_contents($file));
         $this->filename = basename($file);
     }
 
